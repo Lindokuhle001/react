3eea5d0942d292a0be6191c59dac4df97307e226
Lindokuhle shabalala <mlixshaba@gmail.com>
2022-02-27 00:10:27 +0200

    styled game

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -135,7 +135,7 @@ class Game extends React.Component {
       const desc = move ? "Go to move #" + move : "Go to game start";
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>
+          <button className="btn" onClick={() => this.jumpTo(move)}>
             {desc}
           </button>
         </li>
